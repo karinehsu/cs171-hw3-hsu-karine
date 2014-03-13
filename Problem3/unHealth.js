f2b27977ad55fae15962784593463784cc04c0f1
karinehsu <karine.hsu.2012@gmail.com>
2014-03-13 05:56:48 -0400

    Update unHealth.js

--- Problem3/unHealth.js
+++ Problem3/unHealth.js
@@ -4,61 +4,94 @@ var margin = {
     top: 50,
     right: 50,
     bottom: 50,
-    left: 150
+    left: 100
 };
 
 var width = 960 - margin.left - margin.right;
 
-var height = 800 - margin.bottom - margin.top;
-
-
+var height = 900 - margin.bottom - margin.top;
 
 bbOverview = {
-    x: 0,
+    x: 50,
     y: 10,
     w: width,
     h: 50
 };
 
 bbDetail = {
-    x: 0,
+    x: bbOverview.x,
     y: 100,
     w: width,
     h: 300
 };
+dataSet = [];
+var color = d3.scale.category10();
 
-var chart1 = d3.select("#linegraph")
+
+var ranks = [];
+
+d3.csv("UN.csv", function(data) {
+
+
+  var parseDate = d3.time.format("%Y%d%m").parse;
+
+     data.forEach(function(d) {
+      d.date = parseDate(d.date);
+      d.health = +d.health;
+    
+    });
+
+     dataSet = data;
+     console.log(dataSet);
+
+
+  var min_year = d3.min(dataSet,function(d){return d.date;});
+  var max_year = d3.max(dataSet,function(d){return d.date;});
+  var min_health = d3.min(dataSet,function(d){return d.health;});
+  var max_health = d3.max(dataSet,function(d){return d.health;});
+
+  var chart1 = d3.select("#linegraph")
     .append("svg");
 
-var chart2 = d3.select("#areagraph")
+  var chart2 = chart1
     .append("svg");
 
 var x = d3.time.scale()
-    .range([0, bbOverview.w]);
+    .domain([min_year,max_year])
+    .range([bbOverview.x, bbOverview.w]);
+
 
 var y = d3.scale.linear()
-    .range([bbOverview.h + 30, 0]);
+    .domain([0, max_health])
+    .range([bbOverview.h, bbOverview.y]);
+
 
 var x1 = d3.time.scale()
-    .range([0, bbDetail.w+50]);
+    .domain([min_year,max_year])
+    .range([bbDetail.x, bbDetail.w]);
 
 var y1 = d3.scale.linear()
-    .range([bbDetail.h+30, 0]);
+  .domain([0, max_health])  
+  .range([bbDetail.h, bbDetail.y]);
     
 var xAxis = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(d3.time.months,3);
+   
 
 var xAxis1 = d3.svg.axis()
     .scale(x)
-    .orient("bottom");
+    .orient("bottom")
+    .ticks(d3.time.months,3);
 
 var yAxis = d3.svg.axis()
     .scale(y)
-    .orient("left");
+    .orient("left")
+    .ticks(3);
 
 var yAxis1 = d3.svg.axis()
-    .scale(y)
+    .scale(y1)
     .orient("left");
 
 
@@ -70,84 +103,80 @@ var line = d3.svg.line()
 
 var area = d3.svg.area()
     .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.health); });
-
-dataSet = [];
-
-// svg = d3.select("#visUN").append("svg").attr({
-//     width: width + margin.left + margin.right,
-//     height: height + margin.top + margin.bottom
-// }).append("g").attr({
-//         transform: "translate(" + margin.left + "," + margin.top + ")"
-//     });
-
-var parseDate = d3.time.format("%Y%m%d").parse;
-
-
-d3.csv("UN.csv", function(data) {
-
-
-     data.forEach(function(d) {
-      d.date = parseDate(d.date);
-      d.health = +d.health;
-    
-    });
-
-     dataSet = data;
-     console.log(dataSet);
-    
-  x.domain(d3.extent(dataSet, function(d) { return d.date; }));
-  y.domain(d3.extent(dataSet, function(d) { return d.health; }));
-  x1.domain(d3.extent(dataSet, function(d) { return d.date; }));
-  y1.domain(d3.extent(dataSet, function(d) { return d.health; }));
-
+    .y0(bbDetail.h)
+    .y1(function(d) { return y1(d.health); });
 
 
 
   chart1.append("g")
       .attr("class", "x axis")
-      .attr("transform", "translate(0," + .1*height + ")")
+      .attr("transform", "translate(0," + bbOverview.x + ")")
       .call(xAxis);
 
   chart1.append("g")
       .attr("class", "y axis")
-      .call(yAxis)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Women's Health");
+      .call(yAxis);
 
   chart1.append("path")
       .datum(dataSet)
       .attr("class", "line")
       .attr("d", line);
 
-  
-  chart2.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + .1*height + ")")
-      .call(xAxis1);
+  var point = chart1.append("g")
+      .attr("class", "line-point");
 
-  chart2.append("g")
-      .attr("class", "y axis")
-      .call(yAxis1)
-    .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end")
-      .text("Women's Health");
+      point.selectAll('circle')
+      .data(dataSet)
+        .enter().append('circle')
+        .attr("cx", function(d) { return x(d.date) })
+        .attr("cy", function(d) { return y(d.health) })
+        .attr("r", 2)
+        .style("fill", "blue");
+
+        // .style("fill", function(d) { return color(this.parentNode.__data__.name);})
+        // .style("stroke", function(d) { return color(this.parentNode.__data__.name); })
 
   chart2.append("path")
       .datum(dataSet)
       .attr("class", "area")
       .attr("d", area);
 
+  var point = chart2.append("g")
+      .attr("class", "line-point");
+
+      point.selectAll('circle')
+      .data(dataSet)
+        .enter().append('circle')
+        .attr("cx", function(d) { return x(d.date) })
+        .attr("cy", function(d) { return y1(d.health) })
+        .attr("r", 2)
+        .style("fill", "blue");
+
+      chart2.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + bbDetail.h + ")")
+        .call(xAxis1);
+
+      chart2.append("g")
+        .attr("class", "y axis")
+        .call(yAxis1);
+
+  var brush = d3.svg.brush().x(x).on("brush", brushed);
+  
+  chart1.append("g").attr("class", "brush").call(brush)
+  .selectAll("rect").attr({
+  height: bbOverview.h,
+  transform: "translate(0,0)"
+  });
+
 
 });
+
+function brushed() {
+  x.domain(brush.empty() ? x1.domain() : brush.extent());
+  focus.select(".area").attr("d", area);
+  focus.select(".x axis").call(xAxis);
+}
 
 
 
